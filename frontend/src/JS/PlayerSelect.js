7fd968d0bdc75ccccd62289536aab2b701013ed6
lars <larskonermann@gmail.com>
2025-09-12 15:59:13 +0200

    added for loop for players

--- frontend/src/JS/PlayerSelect.js
+++ frontend/src/JS/PlayerSelect.js
@@ -1,10 +1,7 @@
 function PlayerSelect() {
-  document.getElementById("player0").innerHTML =
-    localStorage.getItem("player0");
-  document.getElementById("player1").innerHTML =
-    localStorage.getItem("player1");
-  document.getElementById("player2").innerHTML =
-    localStorage.getItem("player2");
-  document.getElementById("player3").innerHTML =
-    localStorage.getItem("player3");
+  for (let i = 0; i < 4; i++) {
+    document.getElementById(`player${i}`).innerHTML = localStorage.getItem(
+      `player${i}`
+    );
+  }
 }

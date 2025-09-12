5adef308de87d70d593cd3541f47679fdbd9682e
lars <larskonermann@gmail.com>
2025-09-12 15:54:10 +0200

    names on playboard fixed

--- frontend/src/JS/PlayerSelect.js
+++ frontend/src/JS/PlayerSelect.js
@@ -1,3 +1,10 @@
 function PlayerSelect() {
-  console.log(document.querySelectorAll(".player"));
+  document.getElementById("player0").innerHTML =
+    localStorage.getItem("player0");
+  document.getElementById("player1").innerHTML =
+    localStorage.getItem("player1");
+  document.getElementById("player2").innerHTML =
+    localStorage.getItem("player2");
+  document.getElementById("player3").innerHTML =
+    localStorage.getItem("player3");
 }
--- frontend/src/JS/StartScreen.js
+++ frontend/src/JS/StartScreen.js
@@ -1,21 +1,29 @@
 function AddButton() {
   player = document.getElementById("nameInput").value;
+  let count = localStorage.length;
 
   if (player !== "") {
     newListItem = document.createElement("li");
     newButton = document.createElement("button");
     newListItem.innerHTML = player;
+    newButton.value = count;
+    newButton.innerHTML = "X";
 
     newListItem.appendChild(newButton);
     newButton.addEventListener("click", function () {
       this.parentElement.remove();
+      localStorage.removeItem(`player${this.value}`);
     });
     newListItem.classList.add("player");
 
-    newButton.innerHTML = "x";
-
     document.getElementById("players").append(newListItem);
+
+    localStorage.setItem(`player${count}`, player);
 
     document.getElementById("nameInput").value = "";
   }
+}
+
+function clearLocalStorage() {
+  localStorage.clear();
 }

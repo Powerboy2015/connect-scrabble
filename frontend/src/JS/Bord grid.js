c8d0e50aa18df4e653dc917797fe152bd3087e64
mukdembu <mukdembuwan@gmail.com>
2025-09-16 12:29:08 +0200

    idk

--- frontend/src/JS/Bord grid.js
+++ frontend/src/JS/Bord grid.js
@@ -1,10 +1,10 @@
 function makegrid(rows, collums) {
     let grid = []
     for (let createdrows = 0; createdrows < rows; createdrows++) {
-        let row = []
-        for (let createdcollums = 0; createdcollums < collums; createdcollums++) {
-            row.push(null);
-        }
+        let row = new Array(7);
+        // for (let createdcollums = 0; createdcollums < collums; createdcollums++) {
+        //     row.push(null);
+        // }
         grid.push(row);
     }
     console.log(grid)

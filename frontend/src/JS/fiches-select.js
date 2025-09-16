e96b90e671525cd6e65ce74044e44332cf3caaaf
lars <larskonermann@gmail.com>
2025-09-16 21:27:39 +0200

    hele tering bende gefixt

--- frontend/src/JS/fiches-select.js
+++ frontend/src/JS/fiches-select.js
@@ -1,13 +1,13 @@
+function PlayerSelect2() {
+  const fiches = document.querySelectorAll("#fiches .letter");
 
-function PlayerSelect() {
-  const fiches = document.querySelectorAll('#fiches .letter');
+  console.log(fiches);
 
-  fiches.forEach(fiche => {
-    fiche.addEventListener('click', () => {
-      
-      fiches.forEach(f => f.classList.remove('selected'));
-      fiche.classList.add('selected');
+  fiches.forEach((fiche) => {
+    fiche.addEventListener("click", () => {
+      fiches.forEach((f) => f.classList.remove("selected"));
+      fiche.classList.add("selected");
+      console.log("works");
     });
   });
 }
-

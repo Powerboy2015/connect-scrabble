65b0aa58ea2def5d981a692a10f22fd57bf38827
mukdembu <mukdembuwan@gmail.com>
2025-09-16 15:25:37 +0200

    grid nu ook visable

--- frontend/src/JS/Bord grid.js
+++ frontend/src/JS/Bord grid.js
@@ -1,11 +1,22 @@
-function makegrid(rows, collums) {
+function makegrid(rows, columns) {
     let grid = []
+    const board = document.getElementById("gridshit")
+
+    board.innerHTML = "";
+
+    board.style.gridTemplateColumns = `repeat(${columns}, 50px)`;
+    board.style.gridTemplateRows = `repeat(${rows}, 50px)`;
+
     for (let createdrows = 0; createdrows < rows; createdrows++) {
         let row = new Array(7);
-        // for (let createdcollums = 0; createdcollums < collums; createdcollums++) {
-        //     row.push(null);
-        // }
         grid.push(row);
+
+        for (let createdcollums = 0; createdcollums < columns; createdcollums++) {
+            const idk = document.createElement("div");
+            idk.classList.add("idk");
+            board.appendChild(idk);
+        }
+        
     }
     console.log(grid)
     return grid

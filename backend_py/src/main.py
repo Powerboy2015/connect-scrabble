e2dc1f88555e8eb4bb54dee7f24b404d3cc5e797
lars <larskonermann@gmail.com>
2025-09-24 15:47:29 +0200

    add friend

--- backend_py/src/main.py
+++ backend_py/src/main.py
@@ -1,7 +1,8 @@
 from flask import Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
-from sqlalchemy.exc import IntegrityError
+from sqlalchemy import event
+from sqlalchemy.engine import Engine
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db"
@@ -11,6 +12,11 @@ CORS(app, resources={r"*":{
     "origins": ["*"]
 }})
 
+@event.listens_for(Engine, "connect")
+def set_sqlite_pragma(dbapi_connection, connection_record):
+        cursor = dbapi_connection.cursor()
+        cursor.execute("PRAGMA foreign_keys=ON")
+        cursor.close()
 
 class Persons(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -23,25 +29,26 @@ class Persons(db.Model):
 
 class Friends(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    person_id = db.Column(db.String, db.ForeignKey('persons.id'))
-    friend = db.Column(db.String)
+    personId = db.Column(db.Integer, db.ForeignKey('persons.id'))
+    friendId = db.Column(db.Integer, db.ForeignKey('persons.id'))
 
 
+with app.app_context():
+    db.create_all()
+
+#get all friends in db voor test
 @app.route("/getFriends", methods=["GET"])
 def getFriends():
     items = Friends.query.all()
     all = []
     for item in items:
         all.append({
-            "id": item.id,
-            "person_id": item.person_id,
-            "friend": item.friend
+            "personId": item.personId,
+            "friendId": item.friendId
         })
     return jsonify(all)
 
-with app.app_context():
-    db.create_all()
-
+#get alle users in db voor test
 @app.route("/get", methods=["GET"])
 def get():
     items = Persons.query.all()
@@ -58,6 +65,7 @@ def get():
         })
     return jsonify(all)
 
+# get email/wachtwoord voor alle data van die user
 @app.route("/get/<string:email>/<string:password>", methods=["GET"])
 def getUser(email, password):
     person = Persons.query.get_or_404(email)
@@ -73,7 +81,7 @@ def getUser(email, password):
     else:
         return jsonify({"failed"}), 400
     
-
+# delete user 
 @app.route("/delete/<string:email>", methods=["DELETE"])
 def deleteUser(email):
     person = Persons.query.get_or_404(email)
@@ -83,7 +91,7 @@ def deleteUser(email):
 
     return jsonify({"message": "deleted"})
 
-
+# create nieuwe user
 @app.route("/create", methods=["POST"])
 def create_account():
     data = request.get_json()
@@ -93,17 +101,17 @@ def create_account():
     birthDate = data.get("birthDate")
     password = data.get("password") 
     msg = Persons(firstName=firstName, password=password, lastName=lastName, email=email, birthDate=birthDate)
-    
-    
     db.session.add(msg)
     db.session.commit()
     return jsonify({"status": "ok"}), 201
 
-    
-
-
-
-
+# addFriend, persoon... voegt persoon... toe als vriend.
+@app.route("/addFriend/<int:personId>/<int:friendId>", methods=["POST"])
+def addFriend(personId, friendId):
+    friend = Friends(personId = personId, friendId = friendId)
+    db.session.add(friend)
+    db.session.commit()
+    return jsonify({"status": "ok"}), 200
 
 if __name__ == "__main__":
     app.run(debug=True, port=5001)

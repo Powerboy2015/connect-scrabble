09d5b9aab4b782a491da5926179c738250d9331f
Powerboy2015 <338039@student.mboutrecht.nl>
2025-09-26 03:13:47 +0200

    allowing storage of clients
    added functionality that stores connections and renews connections

--- frontend/src/JS/lobby/index.js
+++ frontend/src/JS/lobby/index.js
@@ -2,7 +2,13 @@ import MultiplayerConnection from "../classes/MultiplayerConnection.js";
 
 const socket = new MultiplayerConnection;
 
-socket.sendMessage({"Action":"JoinLobby",
-                    "Payload": {
-                        roomCode: "JSX85K"
-                    }})
+// Ensures that newly conencted users have their UUID saved in cookies.
+socket.registerHandler("NewUserConnected",(/** @type {Message}*/ mesg) => {
+    const UUID = mesg.Payload.id
+    window.sessionStorage.setItem("sessionID",UUID)
+})
+
+
+socket.sendMessage({Action:"JoinLobby", Payload:{
+    roomCode:"JSX205J"
+}})
